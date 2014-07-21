81b3e91c301f86998a6f164e0b7f784da7321947
Cavallinux <pmezzano@gmail.com>
2014-07-21 16:57:04 -0400

    MOD - Refactorizacion de servicios de getpreferences y administracion

---
+++ wsmocks-webapp/src/main/java/com/falabella/ws/services/admin/AdministrationWSImpl.java
@@ -0,0 +1,135 @@
+package com.falabella.ws.services.admin;
+
+import org.springframework.stereotype.Service;
+
+import cl.taisachile.scc.ws.ActivateSoftTokenParamVO;
+import cl.taisachile.scc.ws.ActivateSoftTokenResponseVO;
+import cl.taisachile.scc.ws.AddDailyAmountParamVO;
+import cl.taisachile.scc.ws.AddDailyAmountResponseVO;
+import cl.taisachile.scc.ws.AdministrationWS;
+import cl.taisachile.scc.ws.AssignGridParamVO;
+import cl.taisachile.scc.ws.AssignGridResponseVO;
+import cl.taisachile.scc.ws.AssignTokenParamVO;
+import cl.taisachile.scc.ws.AssignTokenResponseVO;
+import cl.taisachile.scc.ws.ContactoInfoVO;
+import cl.taisachile.scc.ws.CreateSoftTokenParamVO;
+import cl.taisachile.scc.ws.CreateSoftTokenResponseVO;
+import cl.taisachile.scc.ws.DeviceVO;
+import cl.taisachile.scc.ws.GetCellsParamVO;
+import cl.taisachile.scc.ws.GetCellsResponseVO;
+import cl.taisachile.scc.ws.GetTransaParamVO;
+import cl.taisachile.scc.ws.GetTransaResponseVO;
+import cl.taisachile.scc.ws.GetUserParamVO;
+import cl.taisachile.scc.ws.GetUserResponseMGVO;
+import cl.taisachile.scc.ws.GetUserResponseVO;
+import cl.taisachile.scc.ws.LockDeviceParamVO;
+import cl.taisachile.scc.ws.LockDeviceResponseVO;
+import cl.taisachile.scc.ws.UnlockDeviceParamVO;
+import cl.taisachile.scc.ws.UnlockDeviceResponseVO;
+
+@Service("administrationService")
+public class AdministrationWSImpl implements AdministrationWS {
+    //private static int stolen = 0;
+    
+    @Override
+    public GetUserResponseVO getUser(GetUserParamVO getUserParam) {
+	GetUserResponseVO response = new GetUserResponseVO();
+	ContactoInfoVO contactInfo = new ContactoInfoVO();
+	contactInfo.setLabel("EMAIL");
+	contactInfo.setValue("rvillablanca@orangepeople.cl");
+	response.getUserInfo().add(contactInfo);
+
+	DeviceVO device = new DeviceVO();
+	device.setAttemptsRemaining(3);
+	device.setState("CURRENT");
+
+	String userId = getUserParam.getUserVO().getUserId();
+	if ("19".equals(userId) || "13450594k".equalsIgnoreCase(userId) || "176716851".equalsIgnoreCase(userId)) {
+	    device.setType("GRID");
+	    device.setAttemptsRemaining(1);
+	} else if ("12345678".equals(userId)) {
+	    device.setType("OTP");
+	} else if ("159566471".equals(userId)) {
+	    device.setType("OTP");
+	} else if ("222222222".equals(userId)) {
+	    device.setType("OTP_SMS");
+	    //stolen++;
+	    /*
+	     * if (stolen == 5) { device.setState("CANCELED"); stolen = 0; }
+	     */
+	} else {
+	    device.setType("GRID");
+	}
+
+	response.setFullName("Rodrigo Villablanca");
+	response.setErrorCode(0);
+	response.setErrorMessage("Ok");
+	response.setUser(getUserParam.getUserVO());
+	response.getDevices().add(device);
+	return response;
+    }
+
+    @Override
+    public CreateSoftTokenResponseVO createSoftToken(
+	    CreateSoftTokenParamVO params) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public AssignTokenResponseVO assignToken(AssignTokenParamVO params) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public AssignGridResponseVO assignGrid(AssignGridParamVO params) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public GetTransaResponseVO getGeneratedChallengeByClient(
+	    GetTransaParamVO params) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public GetUserResponseMGVO getUserMG(GetUserParamVO getUserParam) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public AddDailyAmountResponseVO addDailyAmount(
+	    AddDailyAmountParamVO addDailyAmountParam) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public ActivateSoftTokenResponseVO activateSoftToken(
+	    ActivateSoftTokenParamVO params) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public LockDeviceResponseVO lockDevice(LockDeviceParamVO params) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public GetCellsResponseVO getCells(GetCellsParamVO params) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public UnlockDeviceResponseVO unlockDevice(UnlockDeviceParamVO params) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+}
---
+++ wsmocks-webapp/src/main/java/com/falabella/ws/services/getpreferences/GetPreferencesServiceImpl.java
@@ -0,0 +1,36 @@
+package com.falabella.ws.services.getpreferences;
+
+import org.springframework.stereotype.Service;
+
+import cl.taisachile.scc.ws.Administrationv2WS;
+import cl.taisachile.scc.ws.GetAvailableDeviceTypesParamVO;
+import cl.taisachile.scc.ws.GetAvailableDeviceTypesResponseVO;
+import cl.taisachile.scc.ws.GetPreferencesParamVO;
+import cl.taisachile.scc.ws.GetPreferencesResponseVO;
+import cl.taisachile.scc.ws.GetUserParamVO;
+import cl.taisachile.scc.ws.GetUserResponseVO;
+import cl.taisachile.scc.ws.SetAuthenticationPreferenceParamVO;
+import cl.taisachile.scc.ws.SetAuthenticationPreferenceResponseVO;
+
+@Service("getPreferencesService")
+public class GetPreferencesServiceImpl implements Administrationv2WS {
+    @Override
+    public GetAvailableDeviceTypesResponseVO getAvailableDeviceTypes(GetAvailableDeviceTypesParamVO params) {
+        return new GetAvailableDeviceTypesResponseVO();
+    }
+    
+    @Override
+    public GetPreferencesResponseVO getPreferences(GetPreferencesParamVO getPreferencesParam) {
+        return new GetPreferencesResponseVO();
+    }
+    
+    @Override
+    public GetUserResponseVO getUser(GetUserParamVO getUserParam) {
+        return new GetUserResponseVO();
+    }
+    
+    @Override
+    public SetAuthenticationPreferenceResponseVO setAuthenticationPreference(SetAuthenticationPreferenceParamVO params) {
+        return new SetAuthenticationPreferenceResponseVO();
+    }
+}
