5412b0966dda54b76706a6a200869de251f6c982
Cavallinux <pmezzano@gmail.com>
2016-05-06 15:47:00 -0300

    ADD - Se agrega funcionalidad de Xstream para serializacion de archivos XML

--- wsmocks-webapp/src/main/java/com/falabella/ws/services/guardarequerimiento/GuardaRequerimientoWFImpl.java
+++ wsmocks-webapp/src/main/java/com/falabella/ws/services/guardarequerimiento/GuardaRequerimientoWFImpl.java
@@ -1,15 +1,34 @@
 package com.falabella.ws.services.guardarequerimiento;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
+import javax.annotation.Resource;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.falabella.ws.beans.wfguardarequerimiento.WFGuardaRequerimientoResponse;
 import com.falabella.ws.service.guardarequerimiento.WSGuardaRequerimientoWF;
+import com.thoughtworks.xstream.XStream;
 
 @Service("guardaRequerimientoWFService")
 public class GuardaRequerimientoWFImpl implements WSGuardaRequerimientoWF {
+    @Resource(name = "wfGuardaReqMarshaller")
+    private XStream marshaller;
+    private static Random random;
+    private static DateFormat formatter;
+    private static Logger logger;
+    
+    static {
+	random = new Random(System.currentTimeMillis());
+	formatter = new SimpleDateFormat("dd/MM/yyyy");
+	logger = LoggerFactory.getLogger(GuardaRequerimientoWFImpl.class);
+    }
 
     @Override
     public String requerimientoWF(String rut, String nombres, String apellidoPATERNO, String apellidoMATERNO,
@@ -17,28 +36,21 @@ public class GuardaRequerimientoWFImpl implements WSGuardaRequerimientoWF {
 	    String tipoREQUERIMIENTO, String concepto, String producto, String tipo, String motivo, String codSEGMENTO,
 	    String areaINGRESO, String codOFICINA, String originador, String tipoFORMULARIO, String comentarios,
 	    String malaATENCION, String ejecMALAATENCION, String sucMALAATENCION, String ejecSOLPGO) {
-	StringBuilder response = new StringBuilder();
-	Random random = new Random(100000);
-	response.append("<WSGuardaSolicitudProd>");
-	response.append("<RESPUESTA>");
-	response.append(random.nextBoolean() ? "0" : "190");
-	response.append("</RESPUESTA>");
-	response.append("<NUM_REQUERIMIENTOWF>");
-	response.append(random.nextLong() + 1);
-	response.append("</NUM_REQUERIMIENTOWF>");
-	response.append("<FECHA_INGRESO>");
-	response.append(new SimpleDateFormat("dd/MM/yyyy").format(new Date()));
-	response.append("</FECHA_INGRESO>");
-	response.append("<FECHA_VENCIMIENTO>");
-	response.append(new SimpleDateFormat("dd/MM/yyyy").format(new Date()));
-	response.append("</FECHA_VENCIMIENTO>");
-	response.append("<DEF_FLUJO>");
-	response.append("Solicitud ingresada");
-	response.append("</DEF_FLUJO>");
-	response.append("<ERROR>");
-	response.append("");
-	response.append("</ERROR>");
-	response.append("</WSGuardaSolicitudProd>");
-	return response.toString();
+	Object[] params = new Object[] { rut, nombres, apellidoPATERNO, apellidoMATERNO, telefonoPARTICULAR, celular,
+		descCIUDAD, codCIUDAD, mail, tipoREQUERIMIENTO, concepto, producto, tipoFORMULARIO, comentarios,
+		malaATENCION, ejecMALAATENCION, sucMALAATENCION, ejecSOLPGO };
+	logger.debug("Realizando requerimiento con los siguientes parametros: {}", params);
+	Date now = new Date();
+	String dateFormatted = formatter.format(now);
+	WFGuardaRequerimientoResponse response = new WFGuardaRequerimientoResponse();
+	boolean acceptedRequest = random.nextBoolean();
+	response.setResponseCode(acceptedRequest ? "0" : "190");
+	response.setRequestNumber(String.valueOf(random.nextLong() + 1));
+	response.setRequestDate(dateFormatted);
+	response.setExpirationDate(dateFormatted);
+	response.setFlowDescription(acceptedRequest ? "Solicitud aceptada" : "Solicitud rechazada");
+	response.setErrorDescription(acceptedRequest ? StringUtils.EMPTY : "Error en la solicitud");
+	logger.debug("Respuesta a requerimiento: {}", marshaller.toXML(response));
+	return marshaller.toXML(response);
     }
 }
